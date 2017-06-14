3b867a1639014cc5217b9500edc1124b7d593687
Jimmy McCrory <jimmy.mccrory@gmail.com>
2017-06-14 14:53:10 -0700

    Verify https connections by default
    The 'insecure' option was being read in as a string without being
    converted to a boolean, preventing it from ever being able to be set to
    False. The default value has also been changed to False, so that
    verification of certificates must be explicitly disabled.
    
    Change-Id: Ic68b870ea8d5e2e3451ea7915407fabb918efe72

--- monitorstack/utils/os_utils.py
+++ monitorstack/utils/os_utils.py
@@ -30,6 +30,7 @@ except ImportError as e:  # pragma: no cover
                      ' Please install "python-openstacksdk".'
                      ' ERROR: %s' % str(e))
 
+from distutils.util import strtobool
 from monitorstack import utils
 
 
@@ -43,7 +44,7 @@ class OpenStack(object):
         :type os_auth_args: dict
         """
         self.os_auth_args = os_auth_args
-        self.verify = self.os_auth_args.get('insecure', True) is False
+        self.verify = strtobool(self.os_auth_args.get('insecure', 'False')) is 0
 
     @property
     def conn(self):
